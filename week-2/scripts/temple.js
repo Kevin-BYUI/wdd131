36e687d395fdb45b1f141eb0d3cf888c1d5f2357
Kevin-BYUI <ogutu24009@byui.edu>
2024-09-30 08:04:37 +0300

    Picture Album completed

---
+++ week-2/scripts/temple.js
@@ -0,0 +1,19 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('copyright').textContent = `©${currentYear} 🎀 Kevin Ouma Ogutu 🎀 Kenya`;
+
+const lastModifiedDate = document.lastModified;
+document.getElementById('salutation').textContent = `Last Modified: ${lastModifiedDate}`;
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', function() {
+    navLinks.classList.toggle('active');
+    
+    // Toggle between hamburger and 'X' symbol
+    if (hamburger.textContent === '☰') {
+        hamburger.textContent = 'X';
+    } else {
+        hamburger.textContent = '☰';
+    }
+});
